ee665135421e88408d4b7a57c5678c78c8259609
dhanikataria <160390265+dhanikataria@users.noreply.github.com>
2025-09-28 20:31:06 -0400

    Delete CICD_pipeline directory

--- CICD_pipeline/scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull dhanikataria/dhani_docker_repo:latest
-
-# Run the Docker image as a container
-echo "hello" | docker run -it dhanikataria/dhani_docker_repo cat
